f293ecc263dc3b539f4ae9081d858bd2b8dcd9cf
Roberto Bonnelly <roberto.bonnellya@gmail.com>
2024-07-02 14:43:20 -0400

    tenancy

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -2,29 +2,34 @@ import "server-only";
 import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
 import { posts } from "./db/schema";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 
 export async function getPosts() {
+    const user = auth();
+
+    if (!user.userId) throw new Error("Unauthorized");
+    
     const posts = await db.query.posts.findMany({
-        orderBy:(model, { asc }) => asc(model.id),
-      });
-      return posts;
+        orderBy: (model, { asc }) => asc(model.id),
+        where: (model, { eq }) => eq(model.userID, user.userId),
+    });
+    return posts;
 }
 
 export async function getPost(id: number) {
-  const user = auth();
-  if (!user.userId) throw new Error ("Unauthorized");
-
-  
-  
-  const post = await db.query.posts.findFirst({
-    where: (model, { eq }) => eq(model.id, id),
-  });
-
-  if (!post) throw new Error("Image not found");
-
-  if (post.userID !== user.userId) throw new Error("Unauthorized");
+    const user = auth();
+    if (!user.userId) throw new Error("Unauthorized");
 
 
-  return post;
+
+    const post = await db.query.posts.findFirst({
+        where: (model, { eq }) => eq(model.id, id),
+    });
+
+    if (!post) throw new Error("Image not found");
+
+    if (post.userID !== user.userId) throw new Error("Unauthorized");
+
+
+    return post;
 }
