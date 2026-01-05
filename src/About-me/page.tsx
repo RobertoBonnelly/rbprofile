5bb97b43dab5ce1e6ca4036921bc199399bff125
RobertoBonnelly <roberto.bonnellya@gmail.com>
2026-01-05 16:25:18 -0400

    feat: styled about-me page

--- src/About-me/page.tsx
+++ src/About-me/page.tsx
@@ -1,21 +1,21 @@
-
+import '../styles/About-me.css'
 
 export default function AboutMe() {
     return (
-        <>
-        <p>Who is Roberto?</p>
+        <div className="description">
+        <p className='title'>Who is Roberto?</p>
         
         <div>I have always been a very curious person, with a special affinity for biology. Since very early, I realized I wanted to make a living of making discoveries for everyone to apply for their institutions, which I believe came from the curiosity itself, from the need of going deep into the roots of my questions, rather than taking the shallow answer. Over the years, I have developed the believe that we should wait until, at least, 25 to choose a career, but at the age I was supposed to, I decided to study biotechnology, a new program that had opened in the country just a year before my high school graduation, this decision involved, not only jumping into an immature program, but also moving out of my town.</div>
 
-        <p>Why did I do that?</p>
+        <p className='title'>Why did I do that?</p>
 
         <div>I wanted to actually get out of my comfort zone: a whole new city, where I knew no one and would have to build relationships and a life by myself, forcing myself into a hard and long mental work. After getting my bachelor's degree in biotechnology, I got the chance to work with Sars-Cov-2, where I also exposed myself to deeper programming knowledge, and woke up my interest in computation.</div>
 
         <div>After this, I decided to enrol into the Data Scientist course of datacamp, as well as my Master in Science of Bioinformatics.</div>
 
-        <p>And now, what?</p>
+        <p className='title'>And now, what?</p>
 
-        <div>I want a data science or full-stack role, where I can gain more depth in my computation knowledge and eventually, apply it on a PhD project about drug design.</div>
-        </>
+        <div>I want a data science or full-stack role, where I can gain more depth in my computation knowledge and eventually, apply it on a PhD project about drug design. If my profile sounds interesting to you, look at the e-commerce redirect below, where you can see my full-stack skills.</div>
+        </div>
     )
 }
