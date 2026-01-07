2904e3d92ee0de14489b2318af190143baf1574e
RobertoBonnelly <roberto.bonnellya@gmail.com>
2026-01-07 11:47:41 -0400

    feat: e-commerce topnav styling

--- src/e-commerce/components/TopNav.tsx
+++ src/e-commerce/components/TopNav.tsx
@@ -2,8 +2,9 @@ import '../../styles/Topnav.css'
 
 export default function ECTopNav() {
     return(
-        <div className='topnav'>
-            <input type="text" />
+        <div className='topnav flex m-t'>
+            <input className='LP-input' type="text" placeholder='Look for your product' />
+            <button className='auth'>authentication</button>
         </div>
     )
 }
